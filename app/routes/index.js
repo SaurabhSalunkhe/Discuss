b9dd613cdc8c95781131ac98e1a44a007c9a356a
Saurabh Salunkhe <salunkhe.s@husky.neu.edu>
2017-03-10 11:32:11 -0500

    Fixed errors, website working now !

---
+++ app/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  var data = req.app.get('appData');
+  var pagePhotos = [];
+  var pageSpeakers = data.speakers;
+
+  
+  res.render('index', {
+    pageTitle: 'Home',
+    artwork: pagePhotos,
+    speakers: pageSpeakers,
+    pageID: 'home'
+  });
+
+});
+
+module.exports = router;
